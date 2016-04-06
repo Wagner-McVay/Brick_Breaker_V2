a5b6f095a0f04e213babf2c62b6921d927128ca2
Reilly McVay <reilly.mcvay@students.aie.edu.au>
2016-04-05 17:28:14 -0700

    v2.2

--- Pong with library/Collision.h
+++ Pong with library/Collision.h
@@ -29,6 +29,63 @@ float get3DAngle(const Vec3 &a, const Vec3 &b)
 	return angle;
 }
 
+bool pointCollision(const Vec2 &a, const Vec2 &b1, const Vec2 &b2)
+{
+
+	float A, B, C, D;
+
+	Vec2 AB = a - b1;
+	Vec2 CD = b2 - b1;
+
+	A = a.x - b1.x;
+	B = a.y - b1.y;
+	C = b2.x - b1.x;
+	D = b2.y - b1.y;
+
+	float para = dot(AB, CD) / dot(CD, CD);
+
+	Vec2 XY = { 0, 0 };
+	float x, y;
+	
+	if (para < 0)
+	{
+		XY = b1;
+	}
+	else if (para > 0)
+	{
+		XY = b2;
+	}
+	else
+	{
+		XY.x = b1.x + para * C;
+		XY.y = b1.y + para * D;
+	}
+
+	Vec2 dif = a - XY;
+	float hypo = magnitude(dif);
+
+	if (hypo < 2)
+	{
+		return true;
+	}
+	else return false;
+}
+
+bool LineCollision(const Vec2 &a1, const Vec2 &a2, const Vec2 &b1, const Vec2 &b2)
+{
+	
+	float A, B, C, D;
+
+	A =
+
+	if ((((pos1.x - dim1.x) <= (pos2.x + dim2.x)) && ((pos1.x + dim1.x) >= (pos2.x - dim2.x))) && (((pos1.y - dim1.y) <= (pos2.y + dim2.y)) && ((pos1.y + dim1.y) >= (pos2.y - dim2.y))))
+	{
+		return true;
+	}
+	else return false;
+}
+
+
 bool BoxCollision(const Vec2 &pos1, const Vec2 &dim1, const Vec2 &pos2, const Vec2 &dim2)
 {
 	if ((((pos1.x - dim1.x) <= (pos2.x + dim2.x)) && ((pos1.x + dim1.x) >= (pos2.x - dim2.x))) && (((pos1.y - dim1.y) <= (pos2.y + dim2.y)) && ((pos1.y + dim1.y) >= (pos2.y - dim2.y))))
